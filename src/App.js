bd03dc388511817c35ec9a9ab8c2255c9b39897a
DianaUzun <dianauzun2006@gmail.com>
2023-06-22 00:16:26 +0300

    add feature: sorting by title/desc

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 import PostForm from './components/PostForm';
-
+import MySelect from './components/UI/select/MySelect';
 
 
 function App() {
   const [posts, setPosts] = useState([
-    {id:1, title:"Javascript", desc:'React Полный курс от А до Я.'},
-    {id:2, title:"Javascript", desc:'React Полный курс от А до Я.'},
+    {id:1, title:"Javascript 2", desc:'Javascript 2'},
+    {id:2, title:"Javascript", desc:'Javascript'},
   ]);  
+
+  const [selectedSort, setSelectedSort] = useState('');
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -23,9 +26,29 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id));
   };
 
+  const sortPosts = (sort) => {
+    console.log(sort);
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a,b) => a[sort].localeCompare(b[sort]) ));
+  };
+
+
   return (
     <div className="App">
         <PostForm create={createPost}/>
+
+        <hr style={{margin:'15px 0'}}/>
+        <div>
+          <MySelect 
+          onChange={sortPosts}
+          value={selectedSort}
+          defaultValue="Sort" 
+          options={[
+            {value:"title", name:"by title"},
+            {value:"desc", name:"by description"},
+          ]}
+          />
+        </div>
 
       {/* Тернарный оператор */}
         {posts.length !== 0 
