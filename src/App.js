1010c78a97a96966848b2368de887f4353f81a32
DianaUzun <dianauzun2006@gmail.com>
2023-06-21 16:34:47 +0300

    func and class components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react';
+import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
+
 
 function App() {
-  const [count, setCount] = useState(0);
   const [value, setValue] = useState('Text in input');
-
-  function inc() {
-    setCount(count+1);
-  }
-
-  function dec() {
-    setCount(count-1);
-  }
-
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h1>{value}</h1>
-      <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
-      <button onClick={inc}>Increment</button>
-      <button onClick={dec}>Decrement</button>
+      {/* <Counter/>
+      <Counter/> */}
+
+      {/* <h1>{value}</h1>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)}/> */}
+
+      <ClassCounter/>
     </div>
   );
 }
