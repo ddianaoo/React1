7da99df6f642170ca77f598223ac72c6b78e5d32
DianaUzun <dianauzun2006@gmail.com>
2023-06-21 16:08:04 +0300

    study - change states

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState('Text in input');
 
   function inc() {
     setCount(count+1);
@@ -17,6 +17,8 @@ function App() {
   return (
     <div className="App">
       <h1>{count}</h1>
+      <h1>{value}</h1>
+      <input type="text" value={value} onChange={event => setValue(event.target.value)}/>
       <button onClick={inc}>Increment</button>
       <button onClick={dec}>Decrement</button>
     </div>
