8b022531c07a97194f1ee27eac31d1071a560fc5
DianaUzun <dianauzun2006@gmail.com>
2023-06-21 17:49:07 +0300

    add styles for elements; work with array; create post-item, post-list components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import React, { useState } from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import PostItem from './components/PostItem';
+import PostList from './components/PostList';
+
 
 
 function App() {
-  const [value, setValue] = useState('Text in input');
+  const [posts, setPosts] = useState([
+    {id:1, title:"Javascript", desc:'React Полный курс от А до Я.'},
+    {id:2, title:"Javascript", desc:'React Полный курс от А до Я.'},
+  ]);  
+  const [posts2, setPosts2] = useState([
+    {id:1, title:"Python", desc:'Python Полный курс от А до Я.'},
+    {id:2, title:"Python", desc:'Python Полный курс от А до Я.'},
+    {id:3, title:"Python", desc:'Python Полный курс от А до Я.'},
+    {id:4, title:"Python", desc:'Python Полный курс от А до Я.'},
+    {id:5, title:"Python", desc:'Python Полный курс от А до Я.'}
+  ]);
+
   return (
     <div className="App">
-      {/* <Counter/>
-      <Counter/> */}
-
-      {/* <h1>{value}</h1>
-      <input type="text" value={value} onChange={event => setValue(event.target.value)}/> */}
-
-      <ClassCounter/>
+        <PostList posts={posts} title='List of Posts1'/>
+        <PostList posts={posts2} title='List of Posts2'/>
     </div>
   );
 }
