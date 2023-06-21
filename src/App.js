6593a0b10f1c905bd7ccf4cfab1b0e058bf0cfde
DianaUzun <dianauzun2006@gmail.com>
2023-06-21 22:56:39 +0300

    change logic with post items

--- src/App.js
+++ src/App.js
@@ -15,30 +15,22 @@ function App() {
     {id:2, title:"Javascript", desc:'React Полный курс от А до Я.'},
   ]);  
 
-  const [title, setTitle] = useState('');
-  const [desc, setDesc] = useState('');
+  const [post, setPost] = useState({title:'', desc:''});
 
   const addNewPost = (e) => {
       e.preventDefault();
       // console.log(title);
       // console.log(desc);
-      const newPost = {
-        id: Date.now(),
-        title, 
-        desc
-      };
-      setPosts([...posts, newPost]);
-      console.log(newPost);
-      setTitle('');
-      setDesc('');
+      setPosts([...posts, {...post, id:Date.now()}]);
+      setPost({title:'', desc:''});
   }
 
   return (
     <div className="App">
         <form>
           {/* Управляемый  компонент */}
-          <MyInput type='text' name="title" placeholder='Title' value={title} onChange={event => setTitle(event.target.value)}/><br></br>
-          <MyInput type='text' name="desc" placeholder='Description' value={desc} onChange={event => setDesc(event.target.value)}/><br></br>
+          <MyInput type='text' name="title" placeholder='Title' value={post.title} onChange={event => setPost({...post, title:event.target.value})}/><br></br>
+          <MyInput type='text' name="desc" placeholder='Description' value={post.desc} onChange={event => setPost({...post, desc:event.target.value})}/><br></br>
           <MyButton onClick={addNewPost}>Create</MyButton>
         </form>
 
