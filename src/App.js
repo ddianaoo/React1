2e2f2b0ce1af86ee39a12e708649047679f44705
DianaUzun <dianauzun2006@gmail.com>
2023-07-08 19:15:09 +0300

    improve auth feature: not redirecting to main page when you tap reboot page

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ import { AuthContext } from './context';
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
 
   useEffect(() =>{
     if (localStorage.getItem('auth')) {
       setIsAuth(true);
     }
+    setLoading(false);
   }, []);
-  
+
   return (
 
-    <AuthContext.Provider value={{isAuth, setIsAuth}}>
+    <AuthContext.Provider value={{isAuth, setIsAuth, isLoading}}>
       <BrowserRouter>
         <Navbar/>
         <AppRouter/>
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,10 +2,15 @@ import React, { useContext } from 'react';
 import {  Route, Routes, Navigate } from 'react-router-dom';
 import {privateRoutes, publicRoutes} from '../Router/routes'
 import { AuthContext } from '../context';
+import Loader from './UI/Loader/Loader';
 
 
 const AppRouter = () => {
-    const {isAuth} = useContext(AuthContext);
+    const {isAuth, isLoading} = useContext(AuthContext);
+
+  if (isLoading) {
+    return  <div style={{display: 'flex', justifyContent:'center', marginTop: 50 }}><Loader/></div>
+  }
 
     return (
 
